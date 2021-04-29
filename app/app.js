18548456e3bea4b0a50deee44a744c3b479e3e56
andrejboskovski <78638715+andrejboskovski@users.noreply.github.com>
2021-04-29 20:52:02 +0200

    trying navbar

--- app/app.js
+++ app/app.js
@@ -1,14 +1,25 @@
 
 
 
-// navbar changing color
+// navbar changing color on scroll
 
 $(function () {
   $(document).scroll(function () {
-    var $nav = $(".navbar-div");
+    var $nav = $(".navbar-whole");
     $nav.toggleClass('scrolled', $(this).scrollTop() > $nav.height());
   });
 });
+
+// Toggle menu opening
+
+$(".toggle-icon").click(function(){
+  $(".toggle").addClass("toggle-click");
+  });
+
+$(".toggle-icon-inside").click(function(){
+  $(".toggle").removeClass("toggle-click");
+  });
+
 
 
 
